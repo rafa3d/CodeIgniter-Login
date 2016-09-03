6d6299e21250952863f918e3ac0dacb2bdc0b4af
Rafa Marruedo <rafa3d@gmail.com>
2016-09-03 17:30:01 +0200

    segunda subida

---
+++ application/controllers/inicio.php
@@ -0,0 +1,15 @@
+<?php 
+if (! defined('BASEPATH')) exit ('No direct script access allowed');
+
+class Inicio extends CI_Controller {
+
+	public function index() {
+	
+		$this ->load->view("inicio_view");
+	}
+
+}
+
+/* End Of file inicio.php */
+/* Location: ./appplie fdffefe….. */
+?>
---
+++ application/views/inicio_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang=“en-US”>
+<head>
+	<meta charset=“UTF-8”>
+	<title>Login</title>
+</head>
+<body>
+	<div id=“container”>
+		<h1>Login</h1>
+		<form action="">
+			<label for="">Usuario:</label>
+			<input type="text" name="user" id="user">
+			<br>
+			<label for="">Password:</label>
+			<input type="password" name="pass" id="pass">
+			<br>
+			<input type="submit" value="Entrar">
+		</form>
+	</div>
+</body>
+</html>
